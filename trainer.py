56333f66778b392c9c93e3baa0cd31829dc23f44
Adrian Ciausu <adrian.ciausu@cegeka.com>
2019-02-12 17:03:13 +0200

    embeding is back

--- trainer.py
+++ trainer.py
@@ -1,30 +1,30 @@
 from __future__ import print_function
 
 import os
-import re
-import string
 
+import keras
+import tensorflow
 from keras.callbacks import LambdaCallback, ModelCheckpoint, EarlyStopping, TensorBoard
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Bidirectional, Activation, Embedding
 from keras.layers import LSTM
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
 from keras.utils import to_categorical
 
 import numpy as np
-import random
-import sys
 import io
 
-
 class Trainer:
-    SEQUENCE_LEN = 10
-    STEP = 1
     EMBEDING_SIZE = 128
 
-    text = None
     model = None
-    words = None
-    vocabulary = None
+    sequence_size = None
+    vocabulary_size = None
+
+    path = "big.txt"
+    corpus_lines = []
+    tokenizer = Tokenizer()
 
     def run(self):
         if not os.path.exists('data'):
@@ -34,44 +34,41 @@ class Trainer:
         if not os.path.exists(os.path.join('data', 'checkpoints')):
             os.mkdir(os.path.join('data', 'checkpoints'))
 
-        path = "big.txt"
-        with io.open(path, encoding='utf-8') as f:
-            self.text = f.read().lower()
+        config = tensorflow.ConfigProto()
+        config.gpu_options.allow_growth = True
+        # config.log_device_placement = True
+        keras.backend.set_session(tensorflow.Session(config=config))
 
-        translator = str.maketrans('', '', string.punctuation)
-        self.words = [re.sub(r'\W+', '', w) for w in self.text.lower().translate(translator).split() if
-                      w.strip() != '' or w == '\n']
-        print('Corpus length in letters:', len(self.text))
-        print('Corpus length in words:', len(self.words))
+        with io.open(self.path, encoding='utf-8') as f:
+            self.corpus = f.read().lower()
+            self.corpus_lines = self.corpus.split('\n')
+            print('Corpus length in letters:', len(self.corpus))
+            print('Corpus length in lines:', len(self.corpus_lines))
 
-        self.vocabulary = sorted(set(self.words))
-        print('Unique words:', len(self.vocabulary))
+            self.tokenizer.fit_on_texts(self.corpus_lines)
+            self.vocabulary_size = len(self.tokenizer.word_index) + 1
+            print('Unique words:', self.vocabulary_size)
 
-        self.word_indices = dict((c, i) for i, c in enumerate(self.vocabulary))
-        self.indices_word = dict((i, c) for i, c in enumerate(self.vocabulary))
+            sequences = []
+            for line in self.corpus_lines:
+                token_list = self.tokenizer.texts_to_sequences([line])[0]
+                for i in range(10, len(token_list), 2):
+                    n_gram_sequence = token_list[:i + 1]
+                    sequences.append(n_gram_sequence)
 
-        batches = []
-        next_words = []
-        for i in range(0, len(self.words) - self.SEQUENCE_LEN, self.STEP):
-            batches.append(self.words[i: i + self.SEQUENCE_LEN])
-            next_words.append(self.words[i + self.SEQUENCE_LEN])
-        batch_size = len(batches)
-        print('batch count:', batch_size)
+            self.sequence_size = max([len(x) for x in sequences])
+            sequences = pad_sequences(sequences, self.sequence_size, padding='pre')
 
-        print('Vectorization...')
-        x = np.zeros((batch_size, self.SEQUENCE_LEN), dtype=np.int32)
-        y = np.zeros((batch_size), dtype=np.int32)
-        for i, sequence in enumerate(batches):
-            for t, word in enumerate(sequence):
-                x[i, t] = self.word_indices[word]
-            y[i] = self.word_indices[next_words[i]]
+            sequences = np.array(sequences)
+            x, y = sequences[:, :-1], sequences[:, -1]
+            y = to_categorical(y, num_classes=self.vocabulary_size)
 
-        self.model = self.build_model()
-        self.model.fit(x, y, validation_split=0.1, batch_size=128, epochs=60, callbacks=self.build_callbacks())
+            self.model = self.build_model()
+            self.model.fit(x, y, validation_split=0.1, batch_size=128, epochs=60, callbacks=self.build_callbacks())
 
     def build_callbacks(self):
         checkpoint_path = "./data/checkpoints/text-generator-epoch{epoch:03d}-sequence%d-" \
-                          "loss{loss:.4f}-acc{acc:.4f}-val_loss{val_loss:.4f}-val_acc{val_acc:.4f}" % self.SEQUENCE_LEN
+                          "loss{loss:.4f}-acc{acc:.4f}-val_loss{val_loss:.4f}-val_acc{val_acc:.4f}" % self.sequence_size
         checkpoint_callback = ModelCheckpoint(checkpoint_path, monitor='val_acc', save_best_only=True)
         print_callback = LambdaCallback(on_epoch_end=self.on_epoch_end)
         early_stopping = EarlyStopping(monitor='val_acc', patience=100)
@@ -81,51 +78,42 @@ class Trainer:
 
     def build_model(self):
         model = Sequential()
-        model.add(Embedding(input_dim=len(self.vocabulary), output_dim=self.EMBEDING_SIZE))
-        model.add(Bidirectional(LSTM(self.EMBEDING_SIZE)))
-        model.add(Dropout(0.2))
-        model.add(Dense(len(self.vocabulary)))
-        model.add(Activation('softmax'))
-        model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=["accuracy"])
+        model = self.add_model_layers(model)
+        print(model.summary())
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=["accuracy"])
 
         return model
 
-    def sample(self, preds):
-        preds = np.asarray(preds).astype('float64')
-        preds = np.log(preds)
-        exp_preds = np.exp(preds)
-        preds = exp_preds / np.sum(exp_preds)
-        probas = np.random.multinomial(1, preds, 1)
+    def add_model_layers(self, model):
+        # Embedding
+        model.add(Embedding(input_dim=self.vocabulary_size, output_dim=self.EMBEDING_SIZE, input_length=self.sequence_size - 1))
+        # Hidden
+        model.add(Bidirectional(LSTM(128)))
+        model.add(Dropout(0.1))
+        # Output
+        model.add(Dense(self.vocabulary_size, activation='softmax'))
 
-        return np.argmax(probas)
+        return model
 
     def on_epoch_end(self, epoch, _):
         print()
         print('----- Generating text after Epoch: %d' % epoch)
 
-        start_index = random.randint(0, len(self.words) - self.SEQUENCE_LEN - 1)
-        generated = []
-        sentence = self.words[start_index: start_index + self.SEQUENCE_LEN]
-        generated += sentence
+        seed = "One ball after another passed over as he approached and"
 
-        print('----- Generating with seed: "' + ' '.join(sentence) + '"')
-        for i in range(40):
-            x_pred = np.zeros((1, self.SEQUENCE_LEN))
-            for t, word in enumerate(sentence):
-                x_pred[0, t] = self.word_indices[word]
+        for _ in range(20):
+            token_list = self.tokenizer.texts_to_sequences([seed])[0]
+            token_list = pad_sequences([token_list], maxlen=self.sequence_size - 1, padding='pre')
+            predicted = self.model.predict_classes(token_list, verbose=0)
 
-            preds = self.model.predict(x_pred, verbose=0)[0]
-            next_index = self.sample(preds)
-            next_word = self.indices_word[next_index]
+            output_word = ""
+            for word, index in self.tokenizer.word_index.items():
+                if index == predicted:
+                    output_word = word
+                    break
+            seed += " " + output_word
 
-            generated.append(next_word)
-            sentence = sentence[1:]
-            sentence.append(next_word)
-
-            sys.stdout.write(next_word + " ")
-            sys.stdout.flush()
-        print()
-
+        print(seed)
 
 if __name__ == '__main__':
     runner = Trainer()
